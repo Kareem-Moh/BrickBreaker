83711936b9b689eff1a564b8c98bda6141ee07c2
Kareem Mohamed <kareem.mohamed@mail.utoronto.ca>
2018-06-07 11:37:47 -0400

    Add Skeleton code for java applet

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Game extends Applet{
+	public void init() {
+		
+	}
+	public void paint(Graphics g) {
+		
+	}
+	public void update(Graphics g) {
+		
+	}
+}
